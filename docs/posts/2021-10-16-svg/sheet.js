161830f41c6ad760d1671f5b68b1eb6cacaec130
Melanie Bacou <mel@mbacou.com>
2021-10-20 12:09:01 +0100

    format

--- docs/posts/2021-10-16-svg/sheet.js
+++ docs/posts/2021-10-16-svg/sheet.js
@@ -5,11 +5,17 @@
 
 var pal = ['#3C8DBC','#DD4B39','#00A65A','#00C0EF','#F39C12','#0073B7','#001F3F','#39CCCC','#3D9970','#01FF70','#FF851B','#F012BE','#605CA8','#D81B60','#111111','#D2D6DE'];
 
-svg
-  .insert("g")
-    .attr("class","anno")
-  .insert("rect")
-   .attr("stroke", "red")
-   .attr("width", 10)
-   .attr("heigh", 10);
+const hw = 400;
+const box = 20;
+const max_value = d3.max(data);
 
+svg
+  .selectAll("rect")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("x", (d, i) => Math.min(40, 40+((hw-40)*d/max_value)) )
+  .attr("y", (_, i) => (i * (box+5)+5) )
+  .attr("width", (d, i) => (hw-40)*Math.abs(d)/max_value )
+  .attr("height", box)
+  .attr("fill", pal[3]);
